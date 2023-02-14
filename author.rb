a9cfba41f60e29cace55f0b021364ebfa3cfdc14
mkaya13 <53873573+mkaya13@users.noreply.github.com>
2023-02-14 13:49:46 +0300

    Applies linter

--- author.rb
+++ author.rb
@@ -1,10 +1,11 @@
-class Author 
-    attr_accessor :first_name, last_name, items
-    attr_reader :id
-    def initialize(first_name, last_name)
-        @id = Random.rand(1..10_000)
-        @first_name = first_name
-        @last_name = last_name
-        @items = []
-    end
+class Author
+  attr_accessor :first_name, last_name, items
+  attr_reader :id
+
+  def initialize(first_name, last_name)
+    @id = Random.rand(1..10_000)
+    @first_name = first_name
+    @last_name = last_name
+    @items = []
+  end
 end
