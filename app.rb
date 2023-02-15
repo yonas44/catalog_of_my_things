6c256d2efb55dd4aad365f30e802eacfec23afcf
V-Blaze <valentine6586@gmail.com>
2023-02-15 21:14:18 +0100

    Create list_all_genres method functionality

--- app.rb
+++ app.rb
@@ -2,7 +2,7 @@ require_relative './modules/music_methods'
 
 class App
   include MusicMethods
-  
+
   attr_reader :options
 
   def initialize
@@ -18,21 +18,16 @@ class App
       '9': method(:add_game),
       '10': method(:exit)
     }
+
+    @music_album_arr = File.exist?(MUSIC_ALBUM_FILE) ? JSON.parse(File.read(MUSIC_ALBUM_FILE)) : []
+    @genre_arr = File.exist?(GENRE_FILE) ? JSON.parse(File.read(GENRE_FILE)) : []
   end
 
   def list_all_books
     puts 'Hello'
   end
 
-  # def list_all_music_albums
-  #   puts 'Hello'
-  # end
-
   def list_all_games
-    puts 'Hello'
-  end
-
-  def list_all_genres
     puts 'Hello'
   end
 
@@ -47,10 +42,6 @@ class App
   def add_book
     puts 'Hello'
   end
-
-  # def add_music_album
-  #   puts 'Hello'
-  # end
 
   def add_game
     puts 'Hello'
--- modules/music_methods.rb
+++ modules/music_methods.rb
@@ -3,67 +3,67 @@ require_relative '../genre'
 require 'json'
 
 module MusicMethods
-    MUSIC_ALBUM_FILE = './store/music_album.json'.freeze
-    GENRE_FILE = './store/genre.json'.freeze
+  MUSIC_ALBUM_FILE = './store/music_album.json'.freeze
+  GENRE_FILE = './store/genre.json'.freeze
 
-    @@music_album_arr = File.empty?(MUSIC_ALBUM_FILE) ? [] : JSON.parse(File.read(MUSIC_ALBUM_FILE))
-    @@genre_arr = []
+  #   @music_album_arr = File.exist?(MUSIC_ALBUM_FILE) ? JSON.parse(File.read(MUSIC_ALBUM_FILE)) : []
+  #   @genre_arr = File.exist?(GENRE_FILE) ? JSON.parse(File.read(GENRE_FILE)) : []
 
-    def list_all_music_albums
-        if (File.empty?(MUSIC_ALBUM_FILE))
-            puts 'No music album to show!'
-        else
-            @@music_album_arr.each_with_index do |item, id|
-            index = id + 1
-                puts "#{index} - Name: #{item["name"]} Artist: #{item["artist"]} Genre: #{item["genre"]}  On Spotify: #{item["on_spotify"]}  "
-            end
-        end
+  def list_all_music_albums
+    if !File.exist?(MUSIC_ALBUM_FILE) || File.empty?(MUSIC_ALBUM_FILE)
+      puts 'No music album to show!'
+    else
+      @music_album_arr.each_with_index do |a, i|
+        id = i + 1
+        puts "#{id} - Name: #{a['name']} Artist: #{a['artist']} Genre: #{a['genre']}  On Spotify: #{a['on_spotify']}"
+      end
     end
+  end
 
-    def add_music_album
-        print 'Enter Album name: '
-        name = gets.chomp
-
-        print 'Enter artist name: '
-        artist = gets.chomp
-
-        print 'Enter publish date [YYYY-MM-DD]: '
-        publish_date = gets.chomp
-
-        print 'Enter genre: '
-        genre = gets.chomp
-
-        print 'Is it on shopify [y/n]: '
-        on_spotify = gets.chomp
-
-        Genre.new(genre)
-        MusicAlbum.new(name, artist, genre, publish_date, on_spotify == 'y')
-
-        preserve_music_date(name, artist, genre, publish_date, on_spotify == 'y')
-
-        puts 'Music Album Created Successfuly'
+  def list_all_genres
+    if !File.exist?(GENRE_FILE) || File.empty?(GENRE_FILE)
+      puts 'No genre to show!'
+    else
+      puts @genre_arr.uniq
     end
+  end
 
-    def preserve_music_date(name, artist, genre, publish_date, on_spotify)
-        @@genre_arr.push(genre)
+  def add_music_album
+    print 'Enter Album name: '
+    name = gets.chomp
 
-        @@music_album_arr.push({
-            name: name,
-            artist: artist,
-            genre: genre,
-            publish_date: publish_date,
-            on_spotify: on_spotify,
-        })
+    print 'Enter artist name: '
+    artist = gets.chomp
 
-        File.write(MUSIC_ALBUM_FILE, JSON.generate(@@music_album_arr))
-    end
+    print 'Enter publish date [YYYY-MM-DD]: '
+    publish_date = gets.chomp
 
-    # def fetch_preserved_data
-    #     if(File.exists?(MUSIC_ALBUM_FILE) && File.empty?(MUSIC_ALBUM_FILE) != true )
-    #         JSON.parse(File.read(MUSIC_ALBUM_FILE))
-    #     else
-    #         return []
-    #     end
-    # end
+    print 'Enter genre: '
+    genre = gets.chomp
 
+    print 'Is it on shopify [y/n]: '
+    on_spotify = gets.chomp
+
+    Genre.new(genre)
+    MusicAlbum.new(name, artist, genre, publish_date, on_spotify == 'y')
+
+    preserve_music_date(name, artist, genre, publish_date, on_spotify == 'y')
+
+    puts 'Music Album Created Successfuly'
+  end
+
+  def preserve_music_date(name, artist, genre, publish_date, on_spotify)
+    @genre_arr.push(genre)
+
+    @music_album_arr.push({
+                            'name' => name,
+                            'artist' => artist,
+                            'genre' => genre,
+                            'publish_date' => publish_date,
+                            'on_spotify' => on_spotify
+                          })
+
+    File.write(MUSIC_ALBUM_FILE, JSON.generate(@music_album_arr))
+    File.write(GENRE_FILE, JSON.generate(@genre_arr))
+  end
 end
